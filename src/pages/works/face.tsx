6d37e33cc9639ea18c2a6c6aece58187ced09602
arata <aualrxse@icloud.com>
2021-09-12 00:14:55 +0900

    ライセンスを表記

--- src/pages/works/face.tsx
+++ src/pages/works/face.tsx
@@ -11,12 +11,13 @@ export default class Index extends Component<Props> {
 
 	constructor(props: Props) {
 		super(props)
+		process.browser && window.addEventListener("wheel", (e) => e.preventDefault(), {passive: false})
 	}
 
     	/**
 	 * canvasセットアップ
 	 */
-	onReadyCanvas = (node: HTMLCanvasElement) => {
+	private onReadyCanvas = (node: HTMLCanvasElement) => {
 		if(node == null) return
 		const canvas = new WebGLFace(node, null, null)
 		canvas.init()
@@ -26,6 +27,24 @@ export default class Index extends Component<Props> {
 		return (
 			<div className={styles.container}>
 				<canvas className={styles.canvas} id="canvas" ref={this.onReadyCanvas} width="1920" height="1080"></canvas>
+				<p
+					style={{
+						position: "absolute",
+						bottom: "10px",
+						right: "10px",
+						color: "white"
+					}}
+				>
+					{/* https://sketchfab.com/3d-models/face-e2b2143b03714b97afc9c87f8d75acb3 */}
+					Model by&nbsp;
+					<a
+						style={{
+							color: "white",
+							borderBottom: "1px solid white"
+						}}
+					 	href="https://sketchfab.com/3d-models/face-e2b2143b03714b97afc9c87f8d75acb3" target="_blank">Suharman Putra - Face</a>
+					&nbsp;/ Adapted.
+				</p>
 			</div>
 		)
 	}
