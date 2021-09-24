5caf8a34b1adaaf75fcf1fb804f68e6b25014154
arata <aualrxse@icloud.com>
2021-09-24 22:56:16 +0900

    フォルダ構成

--- src/components/webgl/utils/material/shader/sample.frag
+++
@@ -1,5 +0,0 @@
-varying vec2 v_uv;
-
-void main() {
-	gl_FragColor = vec4(v_uv.x, v_uv.y, 0.0, 1.0);
-}
--- src/components/webgl/work/teresa/utils/glowBall.ts
+++ src/components/webgl/work/teresa/utils/glowBall.ts
@@ -1,10 +1,10 @@
-import { Mesh, MeshBasicMaterial, SphereBufferGeometry } from "three";
+import { Mesh, MeshBasicMaterial, MeshPhongMaterial, SphereBufferGeometry } from "three";
 
 export default class GlowBall extends Mesh {
 
 	constructor() {
 		const geo: SphereBufferGeometry = new SphereBufferGeometry(1, 20, 10)
-		const mat: MeshBasicMaterial = new MeshBasicMaterial({color: 0xffffff})
+		const mat: MeshPhongMaterial = new MeshPhongMaterial({color: 0xffffff})
 
 		super(geo, mat)
 	}
