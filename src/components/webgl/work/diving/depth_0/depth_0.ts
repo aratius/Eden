66a2d6b2fadbb6c63432ac9c084efe068554b08f
aratius <arata1129matsu@icloud.com>
2021-11-07 17:17:24 +0900

    とりあえず海を追加

--- src/components/webgl/work/diving/depth_0/depth_0.ts
+++ src/components/webgl/work/diving/depth_0/depth_0.ts
@@ -1,17 +1,57 @@
+import { PlaneBufferGeometry, RepeatWrapping, Texture, Vector2, Vector3 } from "three";
 import { CameraSettings, RendererSettings } from "../../../interfaces";
+import { loadTexture } from "../../../utils";
 import WebGLCanvasBase from "../../../utils/template/template";
+import Water from "./utils/water"
 
 export default class WebGLDepth_0 extends WebGLCanvasBase {
+
+  private water: Water = null
+	private readonly surfaceSize: Vector2 = new Vector2(1000, 1000)
 
   constructor(canvas: HTMLCanvasElement, renderer: RendererSettings, camera: CameraSettings) {
     super(canvas, renderer, camera)
   }
 
   _onInit(): void {
+		this.camera.position.set(0, 3, 0)
+
+    this.initWater()
     this.endLoading()
   }
   _onDeInit(): void {}
   _onResize(): void {}
-  _onUpdate(): void {}
+  _onUpdate(): void {
+    if(this.water != null) {
+      (<any>this.water.material).uniforms.time.value = this.elapsedTime;
+    }
+
+  }
+
+  private async initWater(): Promise<void> {
+		const waterGeometry: PlaneBufferGeometry = new PlaneBufferGeometry(this.surfaceSize.x, this.surfaceSize.y, 500, 500)
+		const waterNormals: Texture = await loadTexture("/assets/images/ocean/Water_1_M_Normal.jpg")
+
+		waterNormals.wrapS = waterNormals.wrapT = RepeatWrapping
+
+		this.water = new Water(
+			waterGeometry,
+			{
+				textureWidth: 512*1,
+				textureHeight: 512*1,
+				waterNormals: waterNormals,
+				sunDirection: new Vector3(),
+				sunColor: 0xffffff,
+				waterColor: 0x001e0f,
+				distortionScale: 3.7,
+				fog: this.scene.fog !== undefined
+			}
+		)
+		this.water.rotation.x = - Math.PI / 2
+		this.scene.add(this.water)
+
+		const waterUniforms = (<any>this.water.material).uniforms
+		waterUniforms.size.value = 10
+	}
 
 }
---
+++ src/components/webgl/work/diving/depth_0/utils/material/WaterMat.ts
@@ -0,0 +1,35 @@
+import { Color, Matrix4, ShaderMaterial, UniformsLib, UniformsUtils, Vector3 } from "three";
+import vertShader from "./shader/water.vert"
+import fragShader from "./shader/water.frag"
+
+export default class WaterMaterial extends ShaderMaterial {
+
+    constructor(lights: boolean, side: any, fog: any) {
+        super({
+            uniforms: UniformsUtils.merge( [
+				UniformsLib[ 'fog' ],
+				UniformsLib[ 'lights' ],
+				{
+					'normalSampler': { value: null },
+					'mirrorSampler': { value: null },
+					'alpha': { value: 1.0 },
+					'time': { value: 0.0 },
+					'size': { value: 1.0 },
+					'distortionScale': { value: 20.0 },
+					'textureMatrix': { value: new Matrix4() },
+					'sunColor': { value: new Color( 0x7F7F7F ) },
+					'sunDirection': { value: new Vector3( 0.70707, 0.70707, 0 ) },
+					'eye': { value: new Vector3() },
+					'waterColor': { value: new Color( 0x555555 ) }
+				}
+			] ),
+
+			vertexShader: vertShader,
+			fragmentShader: fragShader,
+			lights: true,
+			side: side,
+			fog: fog
+        })
+    }
+
+}
