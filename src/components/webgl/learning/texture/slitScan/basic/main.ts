3cab68980d0c3c6090691f2ee5a4dc04935eba98
arata <aualrxse@icloud.com>
2022-02-09 19:56:34 +0900

    プレーンを２つ置いた

--- src/components/webgl/learning/texture/slitScan/basic/main.ts
+++ src/components/webgl/learning/texture/slitScan/basic/main.ts
@@ -3,12 +3,14 @@ import WebGLCanvasBase from "../../../../utils/template/template";
 
 export default class WebGLSlitScanBasic extends WebGLCanvasBase {
 
-    private _display: Mesh = null
+    private _realTimeDisplay: Mesh = null
+    private _feedbackDisplay: Mesh = null
     private _video: HTMLVideoElement = null
 
     async _onInit(): Promise<void> {
         await this._initVideo()
-        this._initDisplay()
+        this._initRealTimeDisplay()
+        this._initFeedbackDisplay()
         this.endLoading()
     }
 
@@ -24,16 +26,26 @@ export default class WebGLSlitScanBasic extends WebGLCanvasBase {
 
     }
 
-    private async _initDisplay(): Promise<void> {
+    private async _initFeedbackDisplay(): Promise<void> {
+        const geo = new PlaneBufferGeometry(1000/2, 600/2, 10, 10)
+        const mat = new MeshBasicMaterial({color: 0xff0000})
+        this._feedbackDisplay = new Mesh(geo, mat)
+        this._feedbackDisplay.position.set(-200, -150, 0)
+
+        this.scene.add(this._feedbackDisplay)
+    }
+
+    private async _initRealTimeDisplay(): Promise<void> {
         const texture = new VideoTexture(this._video)
         texture.magFilter = LinearFilter
         texture.minFilter = LinearFilter
         texture.format = RGBFormat
-        const geo = new PlaneBufferGeometry(1280/2, 720/2, 10, 10)
+        const geo = new PlaneBufferGeometry(1000/2, 600/2, 10, 10)
         const mat = new MeshBasicMaterial({color: 0xffffff, map: texture})
 
-        this._display = new Mesh(geo, mat)
-        this.scene.add(this._display)
+        this._realTimeDisplay = new Mesh(geo, mat)
+        this._realTimeDisplay.position.set(200, 150, 0)
+        this.scene.add(this._realTimeDisplay)
     }
 
     private async _initVideo(): Promise<void> {
